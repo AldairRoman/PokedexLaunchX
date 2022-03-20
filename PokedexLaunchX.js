2e6b5fd90fd35338a90402ecc4ba8a1b88b5cc8d
aldair-roman <99059996+AldairRoman@users.noreply.github.com>
2022-03-19 19:28:15 -0600

    se añadio estadisticas y hablidades al pokedex

--- PokedexLaunchX.js
+++ PokedexLaunchX.js
@@ -14,8 +14,11 @@ const fetchpokemon = () => {
         }
     }).then(data => {
         try
-        {
+        {   
             if (data) {
+                let pokeStats=data.stats;
+                //console.log(pokeStats[5].base_stat);
+                let pokeAbility=data.abilities; 
                 let pokeTypes = data.types;
                 let pokeimg = data.sprites.front_default;
                 let pokeId = data.id;
@@ -25,6 +28,8 @@ const fetchpokemon = () => {
                 pokeID(pokeId, data.name);
                 pokeData(pokePeso, pokeAltura);
                 pokeTipos(pokeTypes);
+                pokeAbilities(pokeAbility);
+                pokeStat(pokeStats[0].base_stat,pokeStats[1].base_stat,pokeStats[2].base_stat,pokeStats[5].base_stat);
             }
             else
             {
@@ -52,7 +57,7 @@ const pokeID = (id, namePoke) => {
 }
 
 const pokeData = (peso, altura) => {
-    document.getElementById("pokeData-peso").innerHTML = `Peso: ${peso} kg`;
+    document.getElementById("pokeData-peso").innerHTML = `Peso: ${peso}`;
     document.getElementById("pokeData-altura").innerHTML = `Altura: ${altura} `;
 }
 
@@ -63,4 +68,22 @@ const pokeTipos = (pokeType) => {
     }
     document.getElementById("pokeTypes").innerHTML = `Tipo: ${tipo}`;
 
+}
+
+const pokeAbilities=(pokeability)=>
+{ let movimiento="";
+    for (let i = 0; i < pokeability.length; i++) 
+    {
+        movimiento += " " + pokeability[i].ability.name;
+    }
+    document.getElementById("pokeAbilities").innerHTML = `Movimientos: ${movimiento}`;
+
+}
+
+const pokeStat=(hp,ataque,defensa,speed)=>
+{
+    document.getElementById("pokestat-HP").innerHTML = `HP: ${hp}`;
+    document.getElementById("pokestat-Ataque").innerHTML = `Ataque: ${ataque}`;
+    document.getElementById("pokestat-Defensa").innerHTML = `Defensa: ${defensa}`;
+    document.getElementById("pokestat-Speed").innerHTML = `Speed: ${speed}`;
 }
